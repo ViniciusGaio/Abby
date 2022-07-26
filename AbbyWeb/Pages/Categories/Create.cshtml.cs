b379c5fc0b9e9bd3332e13058ea7356cc958f553
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-26 15:15:07 -0300

    Update: Data Annotations and client side Validation

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -22,6 +22,10 @@ namespace AbbyWeb.Pages.Categories
         }
         public async Task<IActionResult> OnPost()
         {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The Display Order cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 await _context.Categories.AddAsync(Category);
